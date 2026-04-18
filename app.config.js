a351fc90f30c474dba9df75f22fa436e54c6362f
Daniel <daniel.valenz@icloud.com>
2026-04-17 21:00:54 -0500

    codemagic

--- app.config.js
+++ app.config.js
@@ -22,7 +22,7 @@ module.exports = {
 
     ios: {
       bundleIdentifier: "com.beforeitbills.app",
-      buildNumber: "19", //increasing build #
+      buildNumber: "50", //increasing build #
       userInterfaceStyle: "automatic",
       supportsTablet: false,
       config: {
